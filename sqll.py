40b96144b5cd70003ef13b443af1e4b4771fb8b4
nataliepankova <99887495+nataliepankova@users.noreply.github.com>
2022-03-22 11:35:16 +0300

    Add files via upload

---
+++ sqll.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+conn = sqlite3.connect('emaildb.sqlite')
+cur = conn.cursor()
+
+cur.execute('DROP TABLE IF EXISTS Counts')
+cur.execute('CREATE TABLE Counts (org TEXT, count INTEGER)')
+
+fname = 'mbox.txt'
+fh = open(fname)
+for line in fh:
+    if line.startswith("From: "):
+        words = line.split()
+        email = words[1]
+        o = email.find("@")
+        org = email[o+1:]
+        cur.execute('SELECT count FROM Counts WHERE org = ? ', (org,))
+        row = cur.fetchone()
+        if row is None:
+            cur.execute('''INSERT INTO Counts (org, count)
+                VALUES (?, 1)''', (org,))
+        else:
+            cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?',
+                    (org,))
+        conn.commit()
+sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
+for row in cur.execute(sqlstr):
+    print(str(row[0]), row[1])
+
+cur.close()
+
